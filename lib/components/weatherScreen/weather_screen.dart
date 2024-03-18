9d6320ae30ed4a1ff6c81fc2ffb31ed3ede3e829
Matthew <mathonich@yahoo.com>
2024-03-18 16:58:30 -0700

    Refactored out widgets

--- lib/components/weatherScreen/weather_screen.dart
+++ lib/components/weatherScreen/weather_screen.dart
@@ -3,8 +3,10 @@ import 'package:cs492_weather_app/components/weatherScreen/hourly_forecasts.dart
 import 'package:cs492_weather_app/models/weather_forecast.dart';
 import '../../models/user_location.dart';
 import 'package:flutter/material.dart';
-import '../location/location.dart';
-import 'package:weather_icons/weather_icons.dart';
+import 'package:cs492_weather_app/widgets/set_location.dart';
+import 'package:cs492_weather_app/widgets/location_text.dart';
+import 'package:cs492_weather_app/widgets/icon.dart';
+import 'package:cs492_weather_app/widgets/temperature_text.dart';
 
 class WeatherScreen extends StatefulWidget {
   final Function getLocation;
@@ -54,11 +56,26 @@ class ForecastWidget extends StatefulWidget {
 
 class _ForecastWidgetState extends State<ForecastWidget> {
   WeatherForecast? selectedHourlyForecast;
+  bool showHourlyForecast = true; // Flag to track whether to show hourly or daily forecast
 
   void updateCurrentForecast(WeatherForecast forecast) {
     setState(() {
       selectedHourlyForecast = forecast;
     });
+  }
+
+  void toggleForecastType() {
+    setState(() {
+      showHourlyForecast = !showHourlyForecast;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.forecastsHourly.isNotEmpty) {
+      selectedHourlyForecast = widget.forecastsHourly[0];
+    }
   }
 
   @override
@@ -73,16 +90,29 @@ class _ForecastWidgetState extends State<ForecastWidget> {
               location: widget.location,
               selectedHourlyForecast: selectedHourlyForecast,
             ),
-            HourlyForecasts(
-              forecastsHourly: widget.forecastsHourly,
-              onForecastSelected: updateCurrentForecast,
+            ElevatedButton(
+              onPressed: toggleForecastType,
+              child: Text(showHourlyForecast ? 'Show Daily Forecast' : 'Show Hourly Forecast'),
             ),
+            if (showHourlyForecast)
+              HourlyForecasts(
+                forecastsHourly: widget.forecastsHourly,
+                onForecastSelected: updateCurrentForecast,
+              ),
+            if (!showHourlyForecast)
+              DailyForecasts(
+                forecasts: widget.forecasts,
+                onForecastSelected: updateCurrentForecast,
+              ),
           ],
         ),
       ),
     );
   }
 }
+
+
+
 class CurrentForecast extends StatelessWidget {
   final UserLocation location;
   final WeatherForecast? selectedHourlyForecast;
@@ -108,7 +138,7 @@ class CurrentForecast extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: DescriptionWidget(
+            child: IconWidget(
               forecasts: selectedHourlyForecast != null
                   ? [selectedHourlyForecast!]
                   : [],
@@ -121,126 +151,10 @@ class CurrentForecast extends StatelessWidget {
 }
 
 
-class DescriptionWidget extends StatelessWidget {
-  const DescriptionWidget({
-    Key? key,
-    required this.forecasts,
-  }) : super(key: key);
-
-  final List<WeatherForecast> forecasts;
-
-  @override
-  Widget build(BuildContext context) {
-    IconData iconData;
-    Color iconColor;
-
-    // Determine the icon and color based on forecast data
-    if (forecasts.isNotEmpty) {
-      final WeatherForecast firstForecast = forecasts.first;
-      final bool isDaytime = firstForecast.isDaytime;
-      if (firstForecast.shortForecast.contains("Sunny")) {
-        iconData = WeatherIcons.day_sunny;
-        iconColor = Colors.yellow;
-      } else if (firstForecast.shortForecast.contains("Cloud")) {
-        iconData = isDaytime ? WeatherIcons.day_cloudy : WeatherIcons.night_cloudy;
-        iconColor = Colors.grey;
-      } else if (firstForecast.shortForecast.contains("Rain")) {
-        iconData = isDaytime ? WeatherIcons.day_rain : WeatherIcons.night_rain;
-        iconColor = Colors.blue;
-      } else if (firstForecast.shortForecast.contains("Snow")) {
-        iconData = isDaytime ? WeatherIcons.day_snow : WeatherIcons.night_snow;
-        iconColor = Colors.grey;
-      } else if (firstForecast.shortForecast.contains("Clear")) {
-        iconData = isDaytime ? WeatherIcons.day_sunny : WeatherIcons.night_clear;
-        iconColor = Colors.black;
-      }
-      else {
-        iconData = Icons.question_mark;
-        iconColor = Colors.red;
-      }
-    } else {
-      iconData = Icons.question_mark;
-      iconColor = Colors.red;
-    }
-
-    return Expanded(
-      child: Center(
-        child: Icon(iconData, size: 75, color: iconColor),
-      ),
-    );
-  }
-}
 
 
-class TemperatureWidget extends StatelessWidget {
-  const TemperatureWidget({
-    super.key,
-    required this.forecasts,
-  });
 
-  final List<WeatherForecast> forecasts;
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 500,
-      height: 60,
-      child: Center(
-        child: Text('${forecasts.elementAt(0).temperature}º',
-            style: Theme.of(context).textTheme.displayLarge),
-      ),
-    );
-  }
-}
 
-class LocationTextWidget extends StatelessWidget {
-  const LocationTextWidget({
-    super.key,
-    required this.location,
-  });
 
-  final UserLocation location;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Center(
-        child: SizedBox(
-          width: 500,
-          child: Center(
-            child: Text("${location.city}, ${location.state}, ${location.zip}",
-                style: Theme.of(context).textTheme.bodyLarge),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LocationWidget extends StatelessWidget {
-  const LocationWidget({
-    super.key,
-    required this.widget,
-  });
-
-  final WeatherScreen widget;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 400,
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("Requires a location to begin"),
-          ),
-          Location(
-              setLocation: widget.setLocation,
-              getLocation: widget.getLocation),
-        ],
-      ),
-    );
-  }
-}
